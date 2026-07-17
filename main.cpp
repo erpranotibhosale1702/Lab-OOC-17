a2cdf066a223a6916b40ed67041dc45f456581b5
erpranotibhosale1702 <er.pranotibhosale1702@gmail.com>
2026-07-17 07:19:49 +0000

    sum

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 // CSE C 17/7/26
 #include<stdio.h>
 int main(){
-    printf("Hello World!");
+   printf("Hello World!");
     return 0;
 }
---
+++ main2.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,sum;
+    printf("Enter 1st number");
+    scanf("%d",&a);
+    printf("Enter 2nd number");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("The value of sum is %d",sum);
+    return 0;
+}
