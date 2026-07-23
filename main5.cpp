752363feabcacf92d3a7e206426585040ac387ed
erpranotibhosale1702 <er.pranotibhosale1702@gmail.com>
2026-07-23 14:54:45 +0000

    swapping_using_3rdvariable

---
+++ main5.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main(){
+    int a=10;
+    int b=20;
+    int c=a;
+    a=b;
+    b=c;
+    cout<<"The new value of a is: "<< a <<endl;
+    cout<<"The new value of b is: "<< b <<endl;
+}
+
+    
