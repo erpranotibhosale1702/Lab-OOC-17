6389fda0b6a6ecc982806f11f3cee911b36ec540
erpranotibhosale1702 <er.pranotibhosale1702@gmail.com>
2026-07-24 02:48:46 +0000

    factorial

---
+++ program4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int num;
+    int fact=1;
+    printf("Enter the number");
+    scanf("%d",&num);
+    for(int i=1;i<=num;i++){
+        fact=fact*i;
+    }
+    printf("The factorial of number is %d",fact);
+}
