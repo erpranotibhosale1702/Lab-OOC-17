da67d03934f7d315195eeea4bf35e6e4ec7e95c5
erpranotibhosale1702 <er.pranotibhosale1702@gmail.com>
2026-07-31 04:53:31 +0000

    compiling_3files

---
+++ name3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+namespace pranoti
+{
+    int add_sub();
+    int mul_div();
+}
+
+int main()
+{
+    int choice;
+    int a,b;
+    cout << "\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Modulus\n";
+    cout << "Enter choice: ";
+    cin >> choice;
+
+    switch(choice) {
+        case 1: cout << "Result = " << a + b; break;
+        case 2: cout << "Result = " << a - b; break;
+        case 3: cout << "Result = " << a * b; break;
+        case 4: cout << "Result = " << (float)a / b; break;
+        case 5: cout << "Result = " << a % b; break;
+        default: cout << "Invalid choice";
+    }
+    return 0;
+}
