594c32328c02c6b7f1ff83b2eaf5270308d69f2a
erpranotibhosale1702 <er.pranotibhosale1702@gmail.com>
2026-07-31 06:34:25 +0000

    use_of_new_delete

---
+++ new_delete.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int *arr;
+    int size;
+    int sum=0;
+    cout<<"Enter the size of the integer array: ";
+    cin>>size;
+    cout<<"Creating an array of size "<<size<<"..";
+    arr=new int[size];
+   for (int i=0;i<size;i++){
+    cout<<"Enter the element arr[i]";
+    cin>>arr[i];
+   }
+   for(int i=0;i<size;i++){
+      sum=sum+arr[i];
+   }
+   cout<<"Sum = "<<sum<<endl;
+    cout<<"\nDynamic  allocation of memory for array arr is successful." ;
+    delete[] arr;
+    return 0;
+}
